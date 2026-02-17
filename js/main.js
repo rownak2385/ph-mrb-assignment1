d14aeaf4c752459518ba1263f568111297cbfca5
rownak2385 <rownak.hasan.98@gmail.com>
2026-02-17 20:47:53 +0600

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Mobile menu
+const menu = document.getElementById("mobile-menu");
+const openBtn = document.getElementById("menu-toggle");
+const closeBtn = document.getElementById("close-menu");
+
+openBtn.addEventListener("click", () => {
+  menu.style.right = "0";
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.style.right = "-100%";
+});
+
